ccf8934f67f002d6abdaa244a6b08592ea82d430
Rune Nielsen <runenielsen@runbox.com>
2022-03-11 11:21:07 +0100

    removes dead code

--- DAX.EventProcessing.Dispatcher/IToposTypedEventObservable.cs
+++ DAX.EventProcessing.Dispatcher/IToposTypedEventObservable.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using Topos.Config;
-using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using Topos.Config;
 
 namespace DAX.EventProcessing.Dispatcher
 {
     public interface IToposTypedEventObservable<BaseEventType>
     {
         ToposConsumerConfigurer Config(string groupName, Action<StandardConfigurer<global::Topos.IConsumerImplementation>> configure);
-
         Subject<BaseEventType> OnEvent { get; }
     }
 }
