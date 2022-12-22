7ef48d5ee5f03e98a7954fcecb09be6261a1c608
Rune Nielsen <runenielsen@runbox.com>
2022-12-22 12:04:45 +0100

    can now publish new events (#6)

--- DAX.EventProcessing.Dispatcher/ITypedEventObservable.cs
+++ DAX.EventProcessing.Dispatcher/ITypedEventObservable.cs
@@ -4,5 +4,6 @@ namespace DAX.EventProcessing.Dispatcher;
 
 public interface ITypedEventObservable<BaseEventType>
 {
+    void NewEvent(BaseEventType baseEvent);
     Subject<BaseEventType> OnEvent { get; }
 }
